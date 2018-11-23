26838a0d34ed87d533c7d99a6477ea8051f52013
Andreas Östling <andreas.ostling@ica.se>
2018-11-24 00:17:25 +0100

    Weird

--- Ad.py
+++ Ad.py
@@ -1,4 +1,5 @@
 import uuid
+import os
 import json
 import urllib.request
 from bs4 import BeautifulSoup
@@ -9,9 +10,16 @@ class Ad:
         self.ad_description = None
         self.ad_price = None
         self.ad_id = None
+        self.ad_type = None
         #self.ad_image_url = ad_image_url
 
     def update(self):
+        # Proxy Settings for running it at work
+        #proxies = urllib.request.ProxyHandler({"http" : "os.environ['ICA_PROXY']", "https" : "os.environ['ICA_PROXY']"})
+        #opener = urllib.request.build_opener(proxies)
+        #urllib.request.install_opener(opener)
+        
+        # HTML Fetch
         page = urllib.request.urlopen('https://vend.se/')
         first_interesting=SoupStrainer('ul', {'class': 'list'})
         soup = BeautifulSoup(page, 'html.parser', parse_only=first_interesting)
@@ -27,7 +35,7 @@ class Ad:
 
         # Dig down to interesting ad data
         ad_title = parsed_ad.h2.contents[0]
-        ad_description = parsed_ad.find(itemprop="description").contents[0]
+        ad_description = parsed_ad.find(itemprop="description").text
         ad_price = parsed_ad.find(itemprop="price").contents[0]
 
         # Update fields
@@ -35,8 +43,20 @@ class Ad:
         self.ad_description = ad_description
         self.ad_price = ad_price
         self.ad_id = str(uuid.uuid1())
+        self.ad_type = ad_type
+        print(ad_title + ad_description + ad_price + ad_id + ad_type)
+    def interesting_ad(self):
+        keywords = ["roland mks-20", "mutron", "yamaha dx7", "Avalon"]
+        for x in keywords:
+            if x in self.ad_title or x in self.ad_description:
+                print("Ad has relevant keywords!")
+                return True
+            else:
+                return False
+                
     def shouldBePersisted(self):
-        return True
+        if self.ad_type == "typeS" and self.interesting_ad():
+            return True
         # check that ad is not in db already and
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
--- Database_Handler.py
+++ Database_Handler.py
@@ -2,30 +2,30 @@ import Ad
 import re
 import boto3
 import datetime
-
-# Helper class to convert a DynamoDB item to JSON.
-
 # Database
+# java -D"java.library.path=./DynamoDBLocal_lib" -jar DynamoDBLocal.jar -sharedDb
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
 table = dynamodb.Table('Ads')
 
 # Initialize new Ad object
 ad_object = Ad.Ad()
-
 # Update it to latest ad entry. This should be scheduled in future versions.
 ad_object.update()
-
 ad_json = ad_object.toJSON()
 
-table.put_item(
-        Item={
-            'ad_id': ad_object.ad_id,
-            'ad_title': ad_object.ad_title,
-            'ad_description': ad_object.ad_description,
-            'ad_price': ad_object.ad_price,
-            'ad_timestamp': str(datetime.datetime.now()).split('.')[0]
-        }
-    )
-    #add to db.
-# Convert the updated object to JSON
-# TODO, send ad_json to some database.
+# Check if it should be persisted
+if ad_object.shouldBePersisted():
+    table.put_item(
+            Item={
+                'ad_id': ad_object.ad_id,
+                'ad_title': ad_object.ad_title,
+                'ad_description': ad_object.ad_description,
+                'ad_price': ad_object.ad_price,
+                'ad_timestamp': str(datetime.datetime.now()).split('.')[0]
+            }
+        )
+    print("Written to database: " + ad_json)
+else:
+    print("Ad does not look that interesting.")
+    
+print(ad_json)
